94d43e8bbb2040300d1da6b8d533ffe9ba5812f6
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2022-04-23 14:12:31 -0300

    add CHATWOOT_MIGRATE

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 set -e
+
+if [ "$CHATWOOT_MIGRATE" == "true" ] ; then
+  echo "Chatwoot prepare database..."
+  bundle exec rails db:chatwoot_prepare
+fi
+
 echo "Starting app..."
 bundle exec rails s -p ${PORT:-3000} -b 0.0.0.0

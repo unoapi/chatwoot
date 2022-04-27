92229db75bb4b858b5db191a47cfd1f9207087b9
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2022-04-27 10:12:13 -0300

    run migrate

--- bin/sidekiq.sh
+++ bin/sidekiq.sh
@@ -5,6 +5,7 @@ set -e
 if [ "$CHATWOOT_PREPARE" == "true" ] ; then
   echo "Chatwoot prepare app..."
   bundle exec rails db:chatwoot_prepare
+  bundle exec rails db:prepare
 fi
 
 echo "Starting app..."

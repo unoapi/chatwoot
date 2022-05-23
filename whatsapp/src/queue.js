41b814b4399777be3ea06c4b8979849410246dfd
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2022-05-23 19:59:13 -0300

    bull named job

--- whatsapp/src/queue.js
+++ whatsapp/src/queue.js
@@ -4,20 +4,21 @@ const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 export const createQueue = async (name, process) => {
   console.debug(`Create a queue with name ${name}`)
   const queue = new Queue(name, REDIS_URL)
-  queue.process(async (job, done) => {
+  queue.process(async (job) => {
     try {
       const payload = job.data
       console.debug('Process message %s', payload.token, payload.content)
-      await process(payload)
-      await done()
+      await process('message', payload)
+      job.moveToCompleted('done', true)
     } catch (e) {
       console.log(`Error on process message`, e)
+      job.moveToFailed({message: 'job failed'})
     }
   })
   return queue
 }
 
 export const addToQueue = async (queue, payload, attempts) => {
-  queue.add(payload, { attempts, backoff: 10000 })
+  queue.add('message', payload, { attempts, backoff: 10000 })
   console.debug('Enqueued message %s', payload.content)
 } 

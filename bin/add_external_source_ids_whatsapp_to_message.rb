9b5722a1e620cf095babbfcbaa1a8b0dab0d9f61
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2022-06-08 19:11:54 -0300

    fix field external_source_id_whatsapp

--- bin/add_external_source_ids_whatsapp_to_message.rb
+++ bin/add_external_source_ids_whatsapp_to_message.rb
@@ -4,8 +4,8 @@ module AddExternalSourceIdsWhatsappToMessage
   def self.included(base)
     
     base.class_eval do
-      puts ">>>>>>>>>>>>>>>>>>> overrided external_source_ids adding :whastapp"
-      store_accessor :external_source_ids, :whastapp, prefix: :external_source_id
+      puts ">>>>>>>>>>>>>>>>>>> overrided external_source_ids adding :whatsapp"
+      store_accessor :external_source_ids, :whatsapp, prefix: :external_source_id
     end    
   end
 end

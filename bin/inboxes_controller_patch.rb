1646625eaa587f6b703b43a2f3e73df0f7f5b358
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2022-06-28 18:07:37 -0300

    better logs

--- bin/inboxes_controller_patch.rb
+++ bin/inboxes_controller_patch.rb
@@ -26,13 +26,13 @@ module InboxesControllerPatch
 
     def set_contact_inbox_with_contact_id
       return if params[:contact_id].blank?
-      puts ">>>>> set_contact_inbox original first.........."
+      puts ">>>>> set_contact_inbox original first with source_id: #{params[:contact_id]}.........."
       begin
         @contact_inbox = @inbox_channel.inbox.contact_inboxes.find_by!(source_id: params[:contact_id])
       rescue ActiveRecord::RecordNotFound
-        puts ">>>>> try again by contact id.........."
+        puts ">>>>> try again with contact_id: #{params[:contact_id]}.........."
         @contact_inbox = @inbox_channel.inbox.contact_inboxes.find_by!(contact_id: params[:contact_id])
-        puts ">>>>> retrieved #{@contact_inbox}.........."
+        puts ">>>>> retrieved #{@contact_inbox.id}.........."
       end
     end
   end

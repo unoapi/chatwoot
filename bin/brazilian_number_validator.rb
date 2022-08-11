73568e70b0d037a88f88ed2b1b97fa22bfd823b7
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2022-08-11 17:47:08 -0300

    verify exist phone number to validate

--- bin/brazilian_number_validator.rb
+++ bin/brazilian_number_validator.rb
@@ -2,11 +2,13 @@ require 'phonelib'
 
 class BrazilianNumberValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    phone = Phonelib.parse(value)
-    length = phone.local_number.scan(/\d/).join('').length
-    puts "validate number #{value} with data: country #{phone.country} invalid? #{phone.invalid?} type #{phone.type} local_number #{phone.local_number} and length #{length}"
-    if (phone.country == 'BR' && (phone.invalid? || phone.type == :mobile && length < 11))
-      record.errors.add(attribute, (options[:message] || 'Número esta inválido ou é um celular, e nesse caso precisa ter o nono digito!'))
+    unless value.nil?
+      phone = Phonelib.parse(value)
+      length = phone.local_number.scan(/\d/).join('').length
+      puts "validate number #{value} with data: country #{phone.country} invalid? #{phone.invalid?} type #{phone.type} local_number #{phone.local_number} and length #{length}"
+      if (phone.country == 'BR' && (phone.invalid? || phone.type == :mobile && length < 11))
+        record.errors.add(attribute, (options[:message] || 'Número esta inválido ou é um celular, e nesse caso precisa ter o nono digito!'))
+      end
     end
   end
 end

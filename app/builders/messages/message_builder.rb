6b8362196d86fd5f38f8455acf97e00c4ec207fa
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2022-09-10 16:04:51 -0300

    message status progress for whatsapp

--- app/builders/messages/message_builder.rb
+++ app/builders/messages/message_builder.rb
@@ -102,6 +102,15 @@ class Messages::MessageBuilder
     AgentBot.where(account_id: [nil, @conversation.account.id]).find_by(id: @params[:sender_id])
   end
 
+  def status_param
+    @params[:status] = :progress if @params[:status].blank? && @message_type == 'outgoing' && @params[:action] == 'create'  && @conversation.inbox&.whatsapp?
+    @params[:status].present? ? { status: @params[:status] } : {}
+  end
+
+  def source_id_param
+    @params[:source_id].present? ? { source_id: @params[:source_id] } : {}
+  end
+
   def message_params
     {
       account_id: @conversation.account_id,
@@ -114,6 +123,11 @@ class Messages::MessageBuilder
       items: @items,
       in_reply_to: @in_reply_to,
       echo_id: @params[:echo_id]
-    }.merge(external_created_at).merge(automation_rule_id).merge(campaign_id).merge(template_params)
+    }.merge(external_created_at)
+      .merge(automation_rule_id)
+      .merge(campaign_id)
+      .merge(template_params)
+      .merge(status_param)
+      .merge(source_id_param)
   end
 end

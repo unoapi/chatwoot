a550c7270301146a5928be280e2fa9c90fb83488
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2022-09-22 14:08:16 -0300

    inbox find by inbox id

--- app/controllers/public/api/v1/inboxes_controller.rb
+++ app/controllers/public/api/v1/inboxes_controller.rb
@@ -7,6 +7,10 @@ class Public::Api::V1::InboxesController < PublicController
 
   def set_inbox_channel
     @inbox_channel = ::Channel::Api.find_by!(identifier: params[:inbox_id])
+  rescue ActiveRecord::RecordNotFound
+    # rubocop:disable Rails/FindById
+    @inbox_channel = ::Inbox.find_by!(id: params[:inbox_id]).channel
+    # rubocop:enable Rails/FindById
   end
 
   def set_contact_inbox
@@ -14,12 +18,8 @@ class Public::Api::V1::InboxesController < PublicController
 
     begin
       @contact_inbox = @inbox_channel.inbox.contact_inboxes.find_by!(source_id: params[:contact_id])
-    rescue ActiveRecord::RecordNotFound => e
-      begin
-        @contact_inbox = @inbox_channel.inbox.contact_inboxes.find_by!(contact_id: params[:contact_id])
-      rescue ActiveRecord::RecordNotFound
-        raise e
-      end
+    rescue ActiveRecord::RecordNotFound
+      @contact_inbox = @inbox_channel.inbox.contact_inboxes.find_by!(contact_id: params[:contact_id])
     end
   end
 

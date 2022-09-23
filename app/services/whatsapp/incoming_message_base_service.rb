d115634e3ec806b3eddcb9839bc15b7422599f65
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2022-09-23 15:20:08 -0300

    whatsapp group message

--- app/services/whatsapp/incoming_message_base_service.rb
+++ app/services/whatsapp/incoming_message_base_service.rb
@@ -53,7 +53,7 @@ class Whatsapp::IncomingMessageBaseService
       account_id: @inbox.account_id,
       inbox_id: @inbox.id,
       message_type: :incoming,
-      sender: @contact,
+      sender: @sender,
       source_id: @processed_params[:messages].first[:id].to_s
     )
     attach_files
@@ -89,6 +89,7 @@ class Whatsapp::IncomingMessageBaseService
 
     @contact_inbox = contact_inbox
     @contact = contact_inbox.contact
+    @sender = contact_inbox.contact
   end
 
   def conversation_params
--- app/services/whatsapp/incoming_message_whatsapp_cloud_service.rb
+++ app/services/whatsapp/incoming_message_whatsapp_cloud_service.rb
@@ -4,6 +4,29 @@
 class Whatsapp::IncomingMessageWhatsappCloudService < Whatsapp::IncomingMessageBaseService
   private
 
+  def set_contact
+    contact_params = @processed_params[:contacts]&.first
+    return if contact_params.blank?
+
+    if group_message?
+      contact_inbox = ::ContactInboxWithContactBuilder.new(
+        source_id: contact_params[:wa_id],
+        inbox: inbox,
+        contact_attributes: { email: contact_params[:wa_id] }
+      ).perform
+      @sender = ::ContactInboxWithContactBuilder.new(
+        source_id: contact_params[:from_id],
+        inbox: inbox,
+        contact_attributes: { name: contact_params.dig(:profile, :name), phone_number: "+#{@processed_params[:messages].first[:from]}" }
+      ).perform.contact
+
+      @contact_inbox = contact_inbox
+      @contact = contact_inbox.contact
+    else
+      super
+    end
+  end
+
   def processed_params
     @processed_params ||= params[:entry].first['changes'].first['value']
   end
@@ -11,5 +34,15 @@ class Whatsapp::IncomingMessageWhatsappCloudService < Whatsapp::IncomingMessageB
   def download_attachment_file(attachment_payload)
     url_response = HTTParty.get(inbox.channel.media_url(attachment_payload[:id]), headers: inbox.channel.api_headers)
     Down.download(url_response.parsed_response['url'], headers: inbox.channel.api_headers)
+  end
+
+  def message_content(message)
+    content = super(message)
+    group_message? ? "*#{@sender.name}*: #{content}" : content
+  end
+
+  def group_message?
+    contact_params = @processed_params[:contacts]&.first
+    contact_params.present? && contact_params[:wa_id].include?('@g.us')
   end
 end

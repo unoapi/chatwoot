7afa94aa5e0998161fa046ea2743967796fbd2f0
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2022-11-14 10:40:15 -0300

    replace uuid source id with phone_number

--- app/services/whatsapp/send_on_whatsapp_service.rb
+++ app/services/whatsapp/send_on_whatsapp_service.rb
@@ -90,7 +90,13 @@ class Whatsapp::SendOnWhatsappService < Base::SendOnChannelService
   end
 
   def send_session_message
-    message_id = channel.send_message(message.conversation.contact_inbox.source_id, message)
+    uuid_regex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+    phone_number = unless uuid_regex.match?(message.conversation.contact_inbox.source_id)
+      message.conversation.contact_inbox.source_id
+    else
+      message.conversation.contact_inbox.contact.phone_number.sub('+', '')
+    end
+    message_id = channel.send_message(phone_number, message)
     message.update!(source_id: message_id) if message_id.present?
   end
 

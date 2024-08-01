1c09f000235492cb81164f877f6d671a90b84058
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-08-01 17:17:24 -0300

    feat: add campaign report

--- app/javascript/dashboard/routes/dashboard/settings/campaigns/CampaignCard.vue
+++ app/javascript/dashboard/routes/dashboard/settings/campaigns/CampaignCard.vue
@@ -43,6 +43,23 @@ const colorScheme = computed(() => {
     ? 'secondary'
     : 'success';
 });
+
+const campaignReport = computed(() => {
+  const audience = this.campaign.audience;
+  const reports = {
+    total: 0,
+  };
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < audience.length; i++) {
+    const a = audience[i];
+    if (!reports[a.status]) {
+      reports[a.status] = 0;
+    }
+    reports.total += 1;
+    reports[a.status] += 1;
+  }
+  return reports;
+});
 </script>
 
 <template>
@@ -109,6 +126,18 @@ const colorScheme = computed(() => {
         class="w-1/4 mb-1 text-xs text-slate-700 dark:text-slate-500"
       >
         {{ messageStamp(new Date(campaign.scheduled_at), 'LLL d, h:mm a') }}
+      </div>      
+      <div
+        v-if="campaign.inbox.channel_type == 'Channel::Whatsapp'"
+        class="mb-1 text-xs text-slate-700 dark:text-slate-500"
+      >
+        {{ campaignReport }}
+      </div>
+      <div
+        v-if="campaign.inbox.channel_type == 'Channel::Whatsapp'"
+        class="mb-1 text-xs text-slate-700 dark:text-slate-500"
+      >
+        {{ campaignReport }}
       </div>
     </div>
   </div>

650f7df621973af5b3b2f0b502b0e0f826a3c9de
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2024-11-28 10:32:09 -0300

    log error on EventDispatcherJob

--- app/jobs/event_dispatcher_job.rb
+++ app/jobs/event_dispatcher_job.rb
@@ -3,5 +3,9 @@ class EventDispatcherJob < ApplicationJob
 
   def perform(event_name, timestamp, data)
     Rails.configuration.dispatcher.async_dispatcher.publish_event(event_name, timestamp, data)
+  rescue StandardError => e
+    message = (['Error on EventDispatcherJob', "#{self.class} - #{e.class}: #{e.message}"] + e.backtrace).join("\n")
+    Rails.logger.error(message)
+    raise e
   end
 end
